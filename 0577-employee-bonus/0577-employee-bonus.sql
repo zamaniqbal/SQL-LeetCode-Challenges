2ceb1fb3c87ace4811e026ab6eb47fad59349639
Zaman Iqbal <zamaniqbal34@gmail.com>
2026-01-19 23:59:31 +0000

    Time: 611 ms (67.67%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT
+    e.name
+    ,b.bonus
+FROM employee e
+LEFT JOIN bonus b
+    ON e.empid = b.empid
+WHERE 
+    b.bonus < 1000
+    OR b.bonus IS NULL;
