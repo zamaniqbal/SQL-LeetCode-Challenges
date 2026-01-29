3c7d86668d71685a90ebea03a5979f3535c2541d
Zaman Iqbal <zamaniqbal34@gmail.com>
2026-01-29 15:21:18 +0000

    Time: 880 ms (59.92%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,20 @@
+/* Write your T-SQL query statement below */
+WITH cte AS
+    (
+    SELECT
+        *
+        ,RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS ranking
+    FROM products
+    WHERE change_date <= '2019-08-16'
+    )
+SELECT
+    product_id
+    ,new_price AS price
+FROM cte
+WHERE ranking = 1
+UNION
+SELECT
+    product_id
+    ,10 AS price
+FROM products
+WHERE product_id NOT IN (SELECT product_id FROM cte);
